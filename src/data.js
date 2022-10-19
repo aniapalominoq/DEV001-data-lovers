0d192943941ba887793612a6d994a9567cb75e6f
Ania pq <melanpq@gmail.com>
2022-10-19 18:49:08 -0500

    muestra la lista de todo los pokemones 🙂

--- src/data.js
+++ src/data.js
@@ -1,9 +1,18 @@
-// estas funciones son de ejemplo
-export const example = () => {
-  return "hola";
-};
-
-export const anotherExample = () => {
-  return "OMG";
-};
-function filterData(date) {}
+export function showList(alldate) {
+  const arrayListVistaPokemon = [];
+  alldate.forEach((element) => {
+    let imagePokemons = element.img;
+    let numberPokemons = element.num;
+    let namePokemons = element.name;
+    let typePokemons1 = element.type[0];
+    let typePokemons2 = element.type[1];
+    arrayListVistaPokemon.push({
+      foto: imagePokemons,
+      num: numberPokemons,
+      name: namePokemons,
+      type1: typePokemons1,
+      type2: typePokemons2,
+    });
+  });
+  return arrayListVistaPokemon;
+}
--- src/main.js
+++ src/main.js
@@ -1,30 +1,35 @@
-import { example } from "./data.js";
-import date from "./data/pokemon/pokemon.js";
+import dbpokemon from "./data/pokemon/pokemon.js";
+import { showList } from "./data.js";
 
-//código que tenga que ver con mostrar los datos en la pantalla
+// variables que se mandaran a la funciones.
 
-console.log(date);
-console.log(date.pokemon[0]);
+const alldate = dbpokemon.pokemon;
 
-console.log(`numero: ${date.pokemon[0].num}`);
-console.log(`nombre: ${date.pokemon[0].name}`);
-//console.log(`N° generacion:${date.pokemon.generation}`);
-//console.log(`Nombre generacion:${date.pokemon.generation.name}`);
-console.log(`imagen: ${date.pokemon[0].img}`);
-console.log(`descripcion: ${date.pokemon[0].about}`);
-console.log(`peso: ${date.pokemon[0].size.weight}`);
-console.log(`altura: ${date.pokemon[0].size.height}`);
-console.log(`type 1: ${date.pokemon[0].type[0]}`);
-console.log(`type 2: ${date.pokemon[0].type[1]}`);
+//varible del dom
 
-console.log(date.pokemon.length);
+//llamar ala funcion para mostrar
 
-console.log(
-  `${date.pokemon.forEach((element) =>
-    console.log(
-      `numero:${element.num} nombre: ${element.name} tipo 1: ${element.type[0]} tipo 2: ${element.type[1]}`
-    )
-  )}`
-);
+(function () {
+  const array = showList(alldate);
 
-console.log(example);
+  array.forEach((elemt, index) => {
+    let number = index;
+    document.getElementById(
+      `auxilio${number}`
+    ).outerHTML = `<figure class="card pokemon"><a href="#"><img id="imagen" src="${
+      elemt.foto
+    }" alt="la foto de ${
+      elemt.name
+    }" /></a><figcaption><span id="numero_pokemon"><b>N° : ${
+      elemt.num
+    }</b></span><h3>${
+      elemt.name
+    }</h3><div class="detalle-pokemon"><figure><img id="img_tipo1"src=""alt=""width="25px"height="25px"/><figcaption><span id="tipo1">${
+      elemt.type1
+    }</span></figcaption></figure><figure><img id="img_tipo2"src=""alt=""width="25px"height="25px"/><figcaption><span id="tipo2">${
+      elemt.type2
+    }</span></figcaption></figure></div></figcaption></figure><p id="auxilio${
+      index + 1
+    }"></p>`;
+  });
+})();
